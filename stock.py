7cdde38c94f20625808e918912391306086991ff
Gaven <gmc20.mail@gmail.com>
2024-11-28 00:53:33 -0500

    Lets goooo

--- stock.py
+++ stock.py
@@ -1,39 +1,27 @@
 import yfinance as yf
 import pandas as pd 
 import numpy as np
-import seaborn as sb
-import datetime
-import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split 
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVC
-from sklearn import metrics
 from datetime import datetime
 
 #It takes some time for everything to load
-today = datetime.today().strftime('%Y-%m-%d') 
+today = datetime.today().strftime('%Y-%m-%d')#This is to get the current time
 stock_ticker = input("Enter the stock ticker that you would like to search up: ").upper() #This asks for the ticker that the user wants
 data = yf.download(stock_ticker, start = "2016-01-01", end = today)  #This gets the retrieval dates, but there is no way of making the day as present as possible for the end function
+data.reset_index(inplace = True)
 print(data.head())
-print(data.columns)
+print(data.columns) #I need this line and the one above to make sure that the data is being
 #data.to_csv(str(stock_ticker + "_stock_data.csv"))
 
+data['Present_Close'] = data['Adj Close'] #I had trouble with this because I could not have 'Adj Close' as a column by itself 
+data['Previous_Close'] = data['Adj Close'].shift(1)
+data['Next_Close'] = data['Adj Close'].shift(-1)
+data['Price_Change'] = data['Present_Close']- data['Previous_Close']
+data.dropna(inplace=True)#This is to drop any values that don't exist
 
-Adj_Close = data['Adj Close'] #We only want to use the closing prices because we need to keep this clean
-Previous_Close = Adj_Close.shift(1) #There is no way of getting the previous days price, so we are essentially going to have to shift the data to the left one
-Price_Change = Adj_Close - Previous_Close
-Next_Close = Adj_Close.shift(-1)
-
-data_cleaned = pd.DataFrame({
-    'Previous_Close': Previous_Close,
-    'Price_Change': Price_Change,
-    'Next_Close': Next_Close
-}, index = [0])
-
-X = data_cleaned[['Previous_Close', 'Price_Change']]
-Y = data_cleaned['Next_Close']
+print(data['Price_Change'])
 
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = 0.25, random_state = 42)
 model = LinearRegression()
