d21608ebb35733e0ceba3d1ab687ed09a9d8dbf4
Gaven <gmc20.mail@gmail.com>
2024-12-01 20:06:53 -0500

    Ordered everything in chronological order and information can be graphed

--- src/stockprediction.py
+++ src/stockprediction.py
@@ -31,45 +31,30 @@ Y = data['Next_Close']
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = 0.25) #We can do a new random state
 model = LinearRegression() #This is linear regression basically it is a line of best fit(I will do logistic regression next and then average the two values to get something more accurate)
 model.fit(X_train,Y_train)
+y_pred = model.predict(X_test)
 
-y_pred = model.predict(X_test) 
 
-print("\nSample Predictions (Actual vs Predicted):")
-comparison = pd.DataFrame({'Actual': Y_test[:10].values, 'Predicted': y_pred[:10]})
-print(comparison)
+sorted_stocks = pd.DataFrame({'Actual': Y_test, 'Predicted': y_pred})
+sorted_stocks['Date'] = data.loc[Y_test.index, 'Date']
 
-future_days = 0  # Number of days to predict
-future_days = int(input("How many days ahead would you predict?"))
-latest_row = data.iloc[-1]
-future_predictions = []
+sorted_stocks = sorted_stocks.sort_values(by="Date", ascending=True)
 
-for i in range(future_days):
-    latest_previous_close = latest_row['Present_Close']
-    latest_price_change = latest_row['Present_Close'] - latest_row['Previous_Close']
-    
-    # Prepare input for the model
-    future_input = np.array([[latest_previous_close, latest_price_change]])
-    next_pred = model.predict(future_input)[0]
-    future_predictions.append(next_pred)
-    
-    # Update latest_row for the next prediction
-    latest_row = {
-        'Present_Close': next_pred,
-        'Previous_Close': latest_previous_close
-    }
+print("Top Predictions Sorted by Date (Chronological Order):")
+print(sorted_stocks.head(-10))
 
-# Add future predictions to a DataFrame for visualization
-future_dates = pd.date_range(start=data['Date'].iloc[-1] + pd.Timedelta(days=1), periods=future_days)
-future_df = pd.DataFrame({'Date': future_dates, 'Predicted_Close': future_predictions})
-
-# Visualization
 plt.figure(figsize=(10, 6))
-plt.plot(data['Date'], data['Adj Close'], label='Actual Prices', color='blue')
-plt.plot(future_df['Date'], future_df['Predicted_Close'], label='Future Predictions', color='orange', linestyle='--')
-plt.title(f"{stock_ticker} Stock Price Prediction")
-plt.xlabel("Date")
-plt.ylabel("Price")
+plt.plot(sorted_stocks['Date'], sorted_stocks['Actual'], label='Actual', marker='o', color='blue')
+plt.plot(sorted_stocks['Date'], sorted_stocks['Predicted'], label='Predicted', marker='x', color='orange')
+plt.title('Actual vs Predicted Stock Prices (Chronologically Ordered)')
+plt.xlabel('Date')
+plt.ylabel('Stock Price')
 plt.legend()
+plt.xticks(rotation=45)
 plt.show()
+
+
+# days_ahead = 0
+# days_ahead = int(input("How many days ahead would you like to predict? Keep in mind that the later you go, the more inaccurate the numbers may be"))
+
 
 #Name is going to change later
