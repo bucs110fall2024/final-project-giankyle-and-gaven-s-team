1848a7e335c5113967e481fe02c0c74e0abd3fe9
Giankyle Vallarta <gvallarta@binghamton.edu>
2024-12-03 14:46:57 -0500

    updated gui to showcase the graph of the stock

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from stockprediction import stockpredictor  # Import your stock prediction module
 import yfinance as yf
 
 pygame.init()
@@ -25,7 +26,7 @@ user_font = pygame.font.Font(None, 50)
 title_font = pygame.font.Font(None, 48)
 
 # Load background image
-background_image_path = "../assets/stockBackground.png"
+background_image_path = "../assets/stockBackground.png"  # Update this path if necessary
 background_image = pygame.image.load(background_image_path)
 background_image = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -42,9 +43,9 @@ def create_button(x, y, width, height):
 home_button = create_button(SCREEN_WIDTH - 170, SCREEN_HEIGHT - 70, 150, 50)
 end_button = create_button(20, SCREEN_HEIGHT - 70, 150, 50)
 
-game_button = create_button(20, 20, 150, 50)
-watchlist_button = create_button(SCREEN_WIDTH - 170, 20, 150, 50)
-your_stocks_button = create_button(20, SCREEN_HEIGHT - 70, 150, 50)
+game_button = create_button(0, 0, 200, 50)
+watchlist_button = create_button(0, 0, 200, 50)
+your_stocks_button = create_button(0, 0, 200, 50)
 
 # Functions for rendering elements
 def draw_button_with_shadow(surface, color, rect, shadow_color, corner_radius, shadow_offset=5):
@@ -68,16 +69,41 @@ def draw_centered_text(text, font, color, surface, y_offset=0):
 
 # Screen rendering functions
 def draw_home_screen(mouse_pos):
-    draw_centered_text("Welcome to the Stock Predictor Game", title_font, WHITE, screen)
+    welcome_text = "Welcome to the Stock Predictor Game"
+    text_surface = title_font.render(welcome_text, True, WHITE)
+    text_rect = text_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4))
+    screen.blit(text_surface, text_rect)
+
+    button_width = 200
+    button_height = 50
+    vertical_spacing = 20
+    center_x = SCREEN_WIDTH // 2 - button_width // 2
+
+    game_button.topleft = (center_x, SCREEN_HEIGHT // 2 - button_height - vertical_spacing)
+    watchlist_button.topleft = (center_x, SCREEN_HEIGHT // 2)
+    your_stocks_button.topleft = (center_x, SCREEN_HEIGHT // 2 + button_height + vertical_spacing)
+
     draw_button("Game", game_button, BLUE, mouse_pos)
     draw_button("Watchlist", watchlist_button, BLUE, mouse_pos)
     draw_button("Your Stocks", your_stocks_button, BLUE, mouse_pos)
 
 def draw_secondary_screen(title, mouse_pos):
-    draw_centered_text(title, title_font, BLACK, screen)
-    home_color = BLUE if home_button_clicked else GREEN
-    draw_button("Home", home_button, home_color, mouse_pos)
+    draw_centered_text(title, title_font, WHITE, screen, -200)
+    draw_button("Home", home_button, BLUE, mouse_pos)
     draw_button("End", end_button, BRIGHT_RED, mouse_pos)
+
+def draw_your_stocks_screen(mouse_pos):
+    input_box = pygame.Rect(200, 250, 400, 50)
+    pygame.draw.rect(screen, WHITE, input_box)
+    pygame.draw.rect(screen, BLACK, input_box, 2)
+    text_surface = user_font.render(user_input, True, BLACK)
+    screen.blit(text_surface, (input_box.x + 5, input_box.y + 5))
+
+    draw_button("Home", home_button, BLUE, mouse_pos)
+    draw_button("End", end_button, BRIGHT_RED, mouse_pos)
+
+    if response_message:
+        draw_centered_text(response_message, font, WHITE, screen, 100)
 
 def validate_stock_ticker(ticker):
     try:
@@ -86,33 +112,10 @@ def validate_stock_ticker(ticker):
     except Exception:
         return False
 
-def draw_your_stocks_screen(mouse_pos):
-    # draw_centered_text("Your Stocks Screen", title_font, BLACK, screen)
-    
-    # Define the input box rectangle
-    input_box = pygame.Rect(200, 250, 400, 50)
-    
-    # Draw the input box (white background with a black border)
-    pygame.draw.rect(screen, WHITE, input_box)  # White background
-    pygame.draw.rect(screen, BLACK, input_box, 2)  # Black border (2 pixels wide)
-
-    # Draw the text inside the input box
-    text_surface = user_font.render(user_input, True, BLACK)
-    screen.blit(text_surface, (input_box.x + 5, input_box.y + 5))
-    
-    # Draw the buttons
-    home_color = BLUE if home_button_clicked else GREEN
-    draw_button("Home", home_button, home_color, mouse_pos)
-    draw_button("End", end_button, BRIGHT_RED, mouse_pos)
-    
-    # Draw the response message
-    if response_message:
-        draw_centered_text(response_message, font, BLACK, screen, 50)
-
 # Main loop
 running = True
 while running:
-    screen.blit(background_image, (0, 0))  # Draw the background image
+    screen.blit(background_image, (0, 0)) 
     mouse_pos = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
@@ -122,30 +125,29 @@ while running:
             if current_screen == "home":
                 if game_button.collidepoint(event.pos):
                     current_screen = "game"
-                    home_button_clicked = False  # Reset the home button click state
                 elif watchlist_button.collidepoint(event.pos):
                     current_screen = "watchlist"
-                    home_button_clicked = False
                 elif your_stocks_button.collidepoint(event.pos):
                     current_screen = "your_stocks"
-                    home_button_clicked = False
-            elif current_screen == "your_stocks":
-                if home_button.collidepoint(event.pos):
-                    current_screen = "home"
-                    home_button_clicked = True  # Set to True when "Home" is clicked
-                elif end_button.collidepoint(event.pos):
-                    running = False
             else:
                 if home_button.collidepoint(event.pos):
                     current_screen = "home"
-                    home_button_clicked = True  # Set to True when "Home" is clicked
                 elif end_button.collidepoint(event.pos):
                     running = False
         elif event.type == pygame.KEYDOWN:
             if current_screen == "your_stocks":
                 if event.key == pygame.K_RETURN:
-                    response_message = "Valid Stock!" if validate_stock_ticker(user_input) else "Invalid Stock!"
-                    user_input = ""  # Clear the input field after pressing Enter
+                    if validate_stock_ticker(user_input):
+                        response_message = "Fetching data..."
+                        predictor = stockpredictor(user_input)
+                        predictor.data_fetch()
+                        predictor.model_training()
+                        forecast_df = predictor.stock_prediction(5)
+                        predictor.plot_machinelearning_model(forecast_df)
+                        response_message = "Graph generated!"
+                    else:
+                        response_message = "Invalid stock!"
+                    user_input = ""
                 elif event.key == pygame.K_BACKSPACE:
                     user_input = user_input[:-1]
                 else:
